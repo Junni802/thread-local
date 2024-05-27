40029757e296c604f02f7a9afa0d42cac1cebe30
junhyukpark <junni802@naver.com>
2024-05-27 23:42:05 +0900

    Initial setting

---
+++ src/test/java/covy/threadlocal/ThreadLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.threadlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
